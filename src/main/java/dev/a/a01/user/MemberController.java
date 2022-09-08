0aad87527ff8ca683e59e130d64956d5d52196f7
parksiwoong <tldnd5656@naver.com>
2022-09-08 10:52:05 +0900

    setting

--- src/main/java/dev/a/a01/user/MemberController.java
+++ src/main/java/dev/a/a01/user/MemberController.java
@@ -1,10 +1,12 @@
 package dev.a.a01.user;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
+import java.util.HashMap;
 import java.util.Map;
 
 @Controller
@@ -26,5 +28,12 @@ public class MemberController {
         mv.addObject(service.test(vo));
         mv.setViewName("/dev/a/a01/login");
         return mv;
+    }
+
+    public HashMap _member(ModelMap mv, MemberVo vo)throws Exception{
+        HashMap map = new HashMap(); // 나중에 담을 service
+
+
+        return map;
     }
 }
